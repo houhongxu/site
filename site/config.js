d98a45cae61993e38cdb325fe85ebcdfb1919482
HHX <903040380@qq.com>
2023-06-15 18:58:15 +0800

    feat: 初始化项目

---
+++ site/config.js
@@ -0,0 +1,15 @@
+import { defineConfig } from 'hhxpress'
+
+export default defineConfig({
+  title: 'HHX的网站',
+  themeConfig: {
+    nav: {
+      title: 'HHX',
+      items: [
+        { text: '主页', link: '/' },
+        { text: '笔记', link: '/note/intro' },
+        { text: '面试题', link: '/interview/intro' },
+      ],
+    },
+  },
+})
