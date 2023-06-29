fe3a3e0c351e0e02b8b9d418f4c4494bc2ee7777
HHX <903040380@qq.com>
2023-06-29 17:39:42 +0800

    chore: 完成http文章笔记迁移

--- site/config.js
+++ site/config.js
@@ -6,7 +6,6 @@ export default defineConfig({
     nav: {
       title: 'HHX',
       items: [
-        { text: '主页', link: '/' },
         { text: '笔记', link: '/note/0介绍/intro' },
         { text: '面试', link: '/interview/0介绍/intro' },
         { text: '工具', link: '/package/0介绍/intro' },
