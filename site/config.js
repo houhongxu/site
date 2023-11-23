de684a7f2293d9eb3baa8e04330837ba8fcdfad6
HHX <903040380@qq.com>
2023-11-23 10:53:14 +0800

    feat: 将工具整理到收集并更名面试为总结

--- site/config.js
+++ site/config.js
@@ -7,8 +7,7 @@ export default defineConfig({
       title: 'HHX',
       items: [
         { text: '笔记', link: '/笔记/0介绍/intro' },
-        { text: '面试', link: '/面试/0介绍/intro' },
-        { text: '工具', link: '/工具/0介绍/intro' },
+        { text: '总结', link: '/总结/0介绍/intro' },
         { text: '收集', link: '/收集/0介绍/intro' },
         { text: '业务', link: '/业务/0介绍/intro' },
       ],
