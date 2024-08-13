1cbe117458730659ad9e4ce62fc056e7408fffcd
houhongxu <hongxu.hou@goodwe.com>
2024-08-13 14:08:38 +0800

    chore: 更新版本与配置文件

--- zeropress.config.ts
+++ zeropress.config.ts
@@ -2,7 +2,7 @@ import { UserConfig } from "zeropress";
 
 const config: UserConfig = {
   title: "HHX的博客",
-  description: "点击右上角github图标一键部署同款博客",
+  description: "点击github图标一键部署同款博客",
   themeConfig: {
     nav: [
       {
